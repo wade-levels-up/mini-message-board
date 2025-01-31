d8527b35781867366bcbd6d01619b0c4b1d9ffed
Wade Foster <>
2025-01-31 14:56:14 +1100

    Try different connection string

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-    connectionString: ${{ mini_msg_board.DATABASE_URL }}
+    connectionString: process.env.DATABASE_URL
 })
