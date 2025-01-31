bbb35878e1cf4363c7f4aba2af6bfce3b03fcc10
Wade Foster <>
2025-01-31 14:42:42 +1100

    Try different prod db url

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-    connectionString: process.env.PROD_CONNECTION_STRING
+    connectionString: mini_msg_board.DATABASE_URL
 })
