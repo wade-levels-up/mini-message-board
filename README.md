f6060dc528a9b37fef95089e38e3bdf76bc257fc
Wade Foster <>
2025-01-31 19:15:00 +1100

    Update README and refactor code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mini-message-board
+
+This is a project I completed as part of The Odin Project.
+
+The objective of this project was to create a public message board where people could submit a message with a name of their choice. This project uses Railway (PaaS - Platform As A Service) to host the Express application as well as the PostgreSQL database it pulls data from.
+
+Originally the project was created without a database and later on once I learnt enough about PostgreSQL I faced the challenege of integrating it alongside my original app.
+
+Some of the challenges I faced with this project were understanding environment variables, seeding a remote database using a script and refactoring some of the original code to facilitate the data being stored (Specifically handling Dates and storing Dates in SQL).
+
+Overall a very enjoyable project and it helped me consolidate my learnings
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,33 +4,6 @@ const { format } = require("date-fns");
 const db = require('../db/queries');
 const asyncHandler = require('express-async-handler')
 
-// const messages = [
-//   {
-//     id: 1,
-//     text: "Just deployed my first dynamic Express web app! Uhh.. Woo!",
-//     user: "⚡️ Odinite Wade",
-//     added: new Date(),
-//   },
-//   {
-//     id: 2,
-//     text: "@Wade - Cool but... What the hell's that supposed to mean?",
-//     user: "Scatman_9000",
-//     added: new Date(),
-//   },
-//   {
-//     id: 3,
-//     text: "@Scatman_9000, It's a page that changes content based on who is visiting or the data stored on the server at the time the page is displayed. If you submit a message here, it'll change what's stored on the server, which in turn changes what everyone else here sees.",
-//     user: "✌🏻 Odinite George",
-//     added: new Date(),
-//   },
-//   {
-//     id: 4,
-//     text: "@Odinite George - Ohhhhh... Okay! Sure? 😅",
-//     user: "Scatman_9000",
-//     added: new Date(),
-//   },
-// ];
-
 function formatMessagesDate(msgs) {
   return msgs.map((message) => ({
     ...message,
@@ -47,12 +20,6 @@ indexRouter.get("/new", (req, res) => res.render("pages/form"));
 
 indexRouter.post("/new", asyncHandler(async (req, res) => {
   await db.insertMessage(req.body.messageText, req.body.userName)
-  // messages.push({
-  //   id: messages.length + 1,
-  //   text: req.body.messageText,
-  //   user: req.body.userName,
-  //   added: new Date(),
-  // });
   res.redirect("/");
 }));
 
@@ -65,8 +32,8 @@ indexRouter.get("/details", asyncHandler(async (req, res, next) => {
     const formattedMessage = formatMessagesDate(await db.getMessageById(req.query.id));
     res.render("pages/details", { messages: formattedMessage[0] });
   } catch (error) {
-    error.status = 404; // Set the status code
-    next(error); // Pass the error to the error-handling middleware
+    error.status = 404;
+    next(error);
   }
 }));
 
